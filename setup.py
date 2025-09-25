145f1d00d1ab7c46f56973a0a5b3bd530e8d726f
Tyler Sutterley <tsutterl@uw.edu>
2025-09-25 15:02:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,9 @@ with open('version.txt', encoding='utf8') as fh:
     version = fh.read()
 
 # list of all scripts to be included with package
+scripts = []
 for dir in ['access','dealiasing','geocenter','mapping','scripts','utilities']:
-    scripts=[os.path.join(dir,f) for f in os.listdir(dir) if f.endswith('.py')]
+    scripts.extend([os.path.join(dir,f) for f in os.listdir(dir) if f.endswith('.py')])
 scripts.append(os.path.join('gravity_toolkit','grace_date.py'))
 scripts.append(os.path.join('gravity_toolkit','grace_months_index.py'))
 
